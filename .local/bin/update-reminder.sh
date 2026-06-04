851d3086ead4d145d1144da60de2a8f7bdec63fd
ijet <my-ijet@mail.ru>
2026-06-04 08:25:45 +0300

    upd

--- .local/bin/update-reminder.sh
+++ .local/bin/update-reminder.sh
@@ -6,5 +6,8 @@ ACTION=$(notify-send \
   "Время обновить систему")
 
 if [ "$ACTION" = "update" ]; then
-  kitty -e bash -c "yay -Syu; echo; echo 'Нажмите любую кнопку для выхода,[r] для перезагрузки...'; read -n 1 -r KEY; if [ \"\$KEY\" = 'r' ] || [ \"\$KEY\" = 'R' ]; then reboot; fi"
+  kitty -e bash -c 'yay -Syu; echo
+                   echo "Нажмите любую кнопку для выхода,[r] для перезагрузки..."
+                   read -n 1 -r KEY
+                   if [ "$KEY" = "r" ] || [ "$KEY" = "R" ]; then reboot; fi'
 fi
